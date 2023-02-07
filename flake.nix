7ee63eef3e534b4ea45826fe6b5851d273f638d5
heyimnova <git@heyimnova.dev>
2023-02-07 22:29:21 +0000

    Generation 1
    Refactor flake to use flake-utils-plus
    
    hosts/default.nix replaces the functionality of hosts/default.nix

--- flake.nix
+++ flake.nix
@@ -2,24 +2,58 @@
   description = "My NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     nur.url = "github:nix-community/NUR";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, nur, ... }:
-  {
-    nixosConfigurations = (
-      import ./hosts {
-        inherit (nixpkgs) lib;
-        inherit inputs nixpkgs home-manager nur;
-      }
-    );
-  };
-}
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, utils, home-manager, nur, ... }:
+    utils.lib.mkFlake {
+      inherit self inputs;
 
+      channelsConfig.allowUnfree = true;
+
+      sharedOverlays = [
+        nur.overlay
+      ];
+
+      hostDefaults.modules = [
+        ./hosts
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nova.imports = [
+            ./hosts/home.nix
+          ];
+        }
+      ];
+
+      hosts.nova-desktop = {
+        channelName = "nixpkgs-unstable";
+        modules = [
+          ./hosts/nova-desktop
+          home-manager.nixosModules.home-manager {
+            home-manager.users.nova.imports = [
+              ./hosts/nova-desktop/home.nix
+            ];
+          }
+        ];
+      };
+
+      hosts.nova-laptop.modules = [
+        ./hosts/nova-laptop
+        home-manager.nixosModules.home-manager {
+          home-manager.users.nova.imports = [
+            ./hosts/nova-laptop/home.nix
+          ];
+        }
+      ];
+    };
+}
