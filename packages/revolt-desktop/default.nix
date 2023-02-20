a78c8efa1ec030c5eebb97f3f157573521840c7f
heyimnova <git@heyimnova.dev>
2023-02-20 03:54:18 +0000

    rename revolt package to revolt-desktop

--- packages/revolt-desktop/default.nix
+++ packages/revolt-desktop/default.nix
@@ -35,7 +35,7 @@ stdenv.mkDerivation rec {
 
     cp -a ${appimageContents}/{locales,resources} $out/share/${pname}
     cp -a ${appimageContents}/revolt-desktop.desktop $out/share/applications/${pname}.desktop
-    cp -a ${appimageContents}/usr/share/icons $out/share
+    cp -a ${appimageContents}/usr/share/icons $out/share/icons
 
     substituteInPlace $out/share/applications/${pname}.desktop \
       --replace 'Exec=AppRun' 'Exec=${pname}'
@@ -48,14 +48,14 @@ stdenv.mkDerivation rec {
       --add-flags $out/share/${pname}/resources/app.asar
   '';
 
-  meta = {
+  meta = with lib; {
     description = "An open source user-first chat platform";
     homepage = "https://revolt.chat/";
     changelog = "https://github.com/revoltchat/desktop/releases/tag/v${version}";
-    license = lib.licenses.agpl3Only;
-    maintainers = [ lib.maintainers.heyimnova ];
-    platforms = [ "x86_64-linux" ];
-    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    license = licenses.agpl3Only;
+    maintainers = with maintainers; [ heyimnova ];
+    platforms = platforms.linux;
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     mainProgram = "revolt-desktop";
   };
 }
