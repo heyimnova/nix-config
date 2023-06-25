583d9b10ed17cd7c09d6983f4c23144a8c32552e
heyimnova <git@heyimnova.dev>
2023-06-25 02:05:37 +0100

    Disable idea lid switch
    Add openssh to idea
    
    Open 80,443/tcp on idea
    
    Raise net.core.rmem_max to 2500000 on idea

--- hosts/idea/default.nix
+++ hosts/idea/default.nix
@@ -8,11 +8,28 @@
   console.keyMap = "uk";
   i18n.defaultLocale = "en_GB.UTF-8";
   networking.hostName = "idea";
+  services.openssh.enable = true;
   system.stateVersion = "23.05";
   virtualisation.docker.enable = true;
 
-  boot.loader = {
-      efi.canTouchEfiVariables = true;
-      systemd-boot.enable = true;
+  boot = {
+    kernel.sysctl."net.core.rmem_max"=2500000;
+
+    loader = {
+        efi.canTouchEfiVariables = true;
+        systemd-boot.enable = true;
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  services.logind.extraConfig = ''
+    # disable the lid switch
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+    HandleLidSwitchDocked=ignore
+  '';
 }
