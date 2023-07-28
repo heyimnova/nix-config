3431ecf80e9cd9d75461ed2cdd0ec7941d53b67a
heyimnova <git@heyimnova.dev>
2023-07-28 01:29:22 +0100

    Manage fish with home-manager

--- hosts/home.nix
+++ hosts/home.nix
@@ -3,6 +3,7 @@
 {
 	imports = [
 		../secrets/modules/git
+		../secrets/modules/fish
 	];
 
 	home = {
@@ -30,7 +31,6 @@
 			gitnuro
 			helvum
 			mousai
-			nitch
 			nodejs
 			nodePackages.npm
 			unstable.onlyoffice-bin
@@ -42,7 +42,6 @@
 			unstable.shell-genie
 			signal-desktop
 			spotify
-			starship
 			tealdeer
 			unstable.teams-for-linux
 			texworks
@@ -67,6 +66,32 @@
 
 	programs = {
 		home-manager.enable = true;
+		fish = {
+			enable = true;
+			interactiveShellInit = ''
+				${pkgs.starship}/bin/starship init fish | source
+
+				set -x XDG_DATA_HOME $HOME/.local/share
+				set -x XDG_CONFIG_HOME $HOME/.config
+				set -x XDG_STATE_HOME $HOME/.local/state
+				set -x XDG_CACHE_HOME $HOME/.cache
+
+				set -x PATH $PATH $HOME/.local/bin
+
+				set -x ANDROID_HOME "$XDG_DATA_HOME"/android
+				set -x CARGO_HOME "$XDG_DATA_HOME"/cargo
+				set -x CUDA_CACHE_PATH "XDG_CACHE_HOME"/nv
+				set -x INPUTRC "$XDG_DATA_HOME"/readline/inputrc
+				set -x LESSHISTFILE "$XDG_DATA_HOME"/less/history
+				set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME"/npm/npmrc
+				set -x WINEPREFIX "$XDG_DATA_HOME"/wine
+			'';
+			shellAliases = {
+				fish_greeting = "${pkgs.nitch}/bin/nitch";
+				cn = "clear;${pkgs.nitch}/bin/nitch";
+				la = "${pkgs.exa}/bin/exa -la";
+			};
+		};
 		nix-index = {
 			enable = true;
 			enableFishIntegration = true;
