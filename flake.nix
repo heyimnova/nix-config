f6e35508667d103e35169e4f982ed008355669df
heyimnova <git@heyimnova.dev>
2023-10-19 18:05:06 +0100

    Prepare for rewrite
    Build the start of the NixOS host config manager

--- flake.nix
+++ flake.nix
@@ -1,89 +1,45 @@
 {
-  description = "My NixOS system configurations as a flake";
+  description = "NixOS and home-manager configurations as a flake";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs-rolling.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
-    stable.url = "github:nixos/nixpkgs/release-23.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
 
     arkenfox = {
       url = "github:dwarfmaster/arkenfox-nixos";
-      inputs.nixpkgs.follows = "stable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "stable";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Required for home-manager on nixos-unstable systems
-    home-manager-unstable = {
+    home-manager-rolling = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs.follows = "nixpkgs-rolling";
     };
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "unstable";
+      inputs.nixpkgs.follows = "nixpkgs-rolling";
     };
-
-    nixpkgs.follows = "unstable";
   };
 
-  outputs = inputs @ { self, arkenfox, home-manager, home-manager-unstable, lanzaboote, nixpkgs, nur, stable, utils, ... }:
-    utils.lib.mkFlake {
-      inherit self inputs;
-
-      channelsConfig.allowUnfree = true;
-      hostDefaults.modules = [ ./hosts ];
-      sharedOverlays = [ nur.overlay ];
-
-      hosts.aspire = {
-        channelName = "stable";
-
-        modules = [
-          ./hosts/aspire
-
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.users.nova.imports = [ ./hosts/aspire/home.nix ];
-            home-manager.useUserPackages = true;
-          }
-        ];
-      };
-
-      hosts.nova-desktop.modules = [
-        ./hosts/nova-desktop
-        lanzaboote.nixosModules.lanzaboote
-
-        home-manager-unstable.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.users.nova.imports = [
-            ./hosts/nova-desktop/home.nix
-            arkenfox.hmModules.arkenfox
-          ];
-        }
-      ];
-
-      hosts.nova-laptop = {
-        channelName = "stable";
-
-        modules = [
-          ./hosts/nova-laptop
-
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.nova.imports = [
-              ./hosts/nova-laptop/home.nix
-              arkenfox.hmModules.arkenfox
-            ];
-          }
-        ];
-      };
+  outputs = { nixpkgs, nixpkgs-rolling, nur, arkenfox, home-manager, home-manager-rolling, lanzaboote, ... }:
+  let
+    flake-settings = { # Configurable settings for flake location in filesystem and default user
+      location = "$HOME/.flake";
+      user = "nova";
+      userDescription = "Nova";
     };
+  in
+  {
+    nixosConfigurations = (
+      import ./hosts {
+        inherit nixpkgs nixpkgs-rolling nur arkenfox home-manager home-manager-rolling lanzaboote flake-settings;
+      }
+    );
+  };
 }
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,195 +1,59 @@
-# System-wide config for all systems
-{ pkgs, ... }:
+{ nixpkgs
+, nixpkgs-rolling
+, nur
+, arkenfox
+, home-manager
+, home-manager-rolling
+, lanzaboote
+, flake-settings
+}:
 
+let
+  nixpkgsOptions = {
+    overlays = [ nur.overlay ];
+    config.allowUnfree = true;
+  };
+in
 {
-  console.font = "Lat2-Terminus16";
-  documentation.nixos.enable = false;
-  time.timeZone = "Europe/London";
+  aspire = nixpkgs.lib.nixosSystem {
+    modules = [
+      ({ nixpkgs = nixpkgsOptions; })
 
-  boot = {
-    # A better tcp congestion control algorithm
-    kernelModules = [ "tcp_bbr" ];
-
-    # Network hardening from https://mdleom.com/blog/2020/03/04/caddy-nixos-part-2/
-    kernel.sysctl = {
-      # Disable magic SysRq key
-      "kernel.sysrq" = 0;
-      # Ignore ICMP broadcasts to avoid participating in Smurf attacks
-      "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
-      # Ignore bad ICMP errors
-      "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
-      # Reverse-path filter for spoof protection
-      "net.ipv4.conf.default.rp_filter" = 1;
-      "net.ipv4.conf.all.rp_filter" = 1;
-      # SYN flood protection
-      "net.ipv4.tcp_syncookies" = 1;
-      # Do not accept ICMP redirects (prevent MITM attacks)
-      "net.ipv4.conf.all.accept_redirects" = 0;
-      "net.ipv4.conf.default.accept_redirects" = 0;
-      "net.ipv4.conf.all.secure_redirects" = 0;
-      "net.ipv4.conf.default.secure_redirects" = 0;
-      "net.ipv6.conf.all.accept_redirects" = 0;
-      "net.ipv6.conf.default.accept_redirects" = 0;
-      # Do not send ICMP redirects (we are not a router)
-      "net.ipv4.conf.all.send_redirects" = 0;
-      # Do not accept IP source route packets (we are not a router)
-      "net.ipv4.conf.all.accept_source_route" = 0;
-      "net.ipv6.conf.all.accept_source_route" = 0;
-      # Protect against tcp time-wait assassination hazards
-      "net.ipv4.tcp_rfc1337" = 1;
-      # Latency reduction
-      "net.ipv4.tcp_fastopen" = 3;
-      ## Bufferfloat mitigations
-      # Requires >= 4.9 & kernel module
-      "net.ipv4.tcp_congestion_control" = "bbr";
-      # Requires >= 4.19
-      "net.core.default_qdisc" = "cake";
-    };
-  };
-
-  environment = {
-    # Needed to remove perl and nano which I do not need
-    defaultPackages = [];
-    shells = [ pkgs.fish ];
-
-    variables = {
-      EDITOR = "${pkgs.helix}/bin/hx";
-      VISUAL = "${pkgs.helix}/bin/hx";
-    };
-
-    # These environment variables are set on user login
-    sessionVariables = rec {
-      XDG_BIN_HOME = "$HOME/.local/bin";
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_STATE_HOME = "$HOME/.local/state";
-
-      PATH = [ "${XDG_BIN_HOME}" ];
-
-      # Recommendations from xdg-ninja
-      ANDROID_HOME = "${XDG_DATA_HOME}/android";
-      CARGO_HOME = "${XDG_DATA_HOME}/cargo";
-      CUDA_CACHE_PATH = "${XDG_CACHE_HOME}/nv";
-      GNUPGHOME = "${XDG_DATA_HOME}/gnupg";
-      INPUTRC = "${XDG_DATA_HOME}/readline/inputrc";
-      _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${XDG_CONFIG_HOME}/java";
-      LESSHISTFILE = "${XDG_DATA_HOME}/less/history";
-      NPM_CONFIG_USERCONFIG = "${XDG_CONFIG_HOME}/npm/npmrc";
-      WINEPREFIX = "${XDG_DATA_HOME}/wine";
-      XCOMPOSECACHE = "${XDG_CACHE_HOME}/X11/xcompose";
-    };
-
-    systemPackages = with pkgs; [
-      bat
-      curl
-      eza
-      helix
-      neovim
-      pciutils
-      rsync
-      strace
-      tmux
-      unzip
-      wget
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+      }
     ];
   };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
+  nova-desktop = nixpkgs-rolling.lib.nixosSystem {
+    modules = [
+      lanzaboote.nixosModules.lanzaboote
+      ({ nixpkgs = nixpkgsOptions; })
 
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 3d";
-    };
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
 
-    settings = {
-      auto-optimise-store = true;
-
-      experimental-features = [
-        "flakes"
-        "nix-command"
-      ];
-    };
-  };
-
-  programs = {
-    fish.enable = true;
-    # Needed to completely remove nano
-    nano.syntaxHighlight = false;
-  };
-
-  services = {
-    cron.enable = true;
-    fwupd.enable = true;
-
-    avahi = {
-      enable = true;
-      nssmdns = true;
-
-      publish = {
-        addresses = true;
-        enable = true;
-        userServices = true;
-      };
-    };
-  };
-
-  system.autoUpgrade = {
-    dates = "05:00";
-    enable = true;
-    # Assuming this repo is symlinked to (or in) /etc/nixos
-    flake = "/etc/nixos";
-    # Allow the service to catch up on updates if the system was powered down
-    persistent = true;
-
-    # Update all flake inputs so new packages are installed
-    flags = [
-      "--update-input"
-      "arkenfox"
-      "--update-input"
-      "home-manager"
-      "--update-input"
-      "home-manager-unstable"
-      "--update-input"
-      "lanzaboote"
-      "--update-input"
-      "nur"
-      "--update-input"
-      "stable"
-      "--update-input"
-      "unstable"
-      "--update-input"
-      "utils"
+        home-manager.users.${flake-settings.user}.imports = [
+          arkenfox.hmModules.arkenfox
+        ];
+      }
     ];
   };
 
-  systemd.extraConfig = ''
-    # Faster shutdowns
-    DefaultTimeoutStopSec=10s
-  '';
+  nova-laptop = nixpkgs.lib.nixosSystem {
+    modules = [
+      ({ nixpkgs = nixpkgsOptions; })
 
-  users = {
-    defaultUserShell = pkgs.fish;
-    mutableUsers = false;
-    # Disable root password
-    users.root.hashedPassword = "*";
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
 
-    users.nova = {
-      description = "Nova";
-      isNormalUser = true;
-      useDefaultShell = true;
-    };
+        home-manager.users.${flake-settings.user}.imports = [
+          arkenfox.hmModules.arkenfox
+        ];
+      }
+    ];
   };
 }
